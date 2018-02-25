bc25abbf9b481c6bec4eaf1b220332f3043ec6e0
SGCMarkus <markusornik@gmail.com>
2018-02-25 15:07:20 +0100

    initial blobs
    Change-Id: Ia3a1ea36de9585077c26e8c8ba452504e8e1ff20

---
+++ proprietary/vendor/etc/init/hw/display_port_permission.sh
@@ -0,0 +1,55 @@
+#!/system/bin/sh
+
+# Setup display nodes & permissions
+# HDMI can be fb1 or fb2
+# Loop through the sysfs nodes and determine
+# the HDMI(dtv panel)
+
+function set_perms() {
+    #Usage set_perms <filename> <ownership> <permission>
+    chown -h $2 $1
+    chmod $3 $1
+}
+
+function setHDMIPermission() {
+   file=/sys/class/graphics/fb$1
+   dev_file=/dev/graphics/fb$1
+   dev_gfx_hdmi=/devices/virtual/switch/hdmi
+
+   set_perms $file/hpd system.graphics 0664
+   set_perms $file/res_info system.graphics 0664
+   set_perms $file/vendor_name system.graphics 0664
+   set_perms $file/product_description system.graphics 0664
+   set_perms $file/video_mode system.graphics 0664
+   set_perms $file/format_3d system.graphics 0664
+   set_perms $file/s3d_mode system.graphics 0664
+   set_perms $file/dynamic_fps system.graphics 0664
+   set_perms $file/msm_fb_dfps_mode system.graphics 0664
+   set_perms $file/hdr_stream system.graphics 0664
+   set_perms $file/cec/enable system.graphics 0664
+   set_perms $file/cec/logical_addr system.graphics 0664
+   set_perms $file/cec/rd_msg system.graphics 0664
+   set_perms $file/pa system.graphics 0664
+   set_perms $file/cec/wr_msg system.graphics 0600
+   set_perms $file/hdcp/tp system.graphics 0664
+   set_perms $file/hdmi_audio_cb audioserver.audio 0600
+   ln -s $dev_file $dev_gfx_hdmi
+}
+
+# check for HDMI connection
+for fb_cnt in 0 1 2 3
+do
+    file=/sys/class/graphics/fb$fb_cnt/msm_fb_panel_info
+    if [ -f "$file" ]
+    then
+      cat $file | while read line; do
+        case "$line" in
+            *"is_pluggable"*)
+             case "$line" in
+                  *"1"*)
+                  setHDMIPermission $fb_cnt
+             esac
+        esac
+      done
+    fi
+done
